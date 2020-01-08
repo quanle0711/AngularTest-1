ad5e22a4262b53cf7f0bd9808a69671199cb1f26
quanle0711 <quanleboy7@gmail.com>
2020-01-09 08:46:46 +1300

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import {AfterViewInit, Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import 'jarallax';
+declare var jarallax: any;
 @Component({
-  selector: 'app-root',
+selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'your-local';
+
+
+  constructor(private changeDetector: ChangeDetectorRef) {
+  }
+
+
+
+  ngAfterViewInit() {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.2
+    });
+    
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    jarallax(document.querySelector('.jarallax-my-video'), {
+      speed: 0.2,
+      videoSrc: 'https://vimeo.com/groups/freehd/videos/153749650'
+    });
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { ButtonsModule, WavesModule, CollapseModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonsModule,
+    WavesModule,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
